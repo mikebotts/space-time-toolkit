623a3d967fcaf40307c17fd140f7e2ea5070efc3
tcook <tcook@813d399c-aa55-0410-8b7d-c12975737b24>
2006-01-23 05:32:39 +0000

    Repackaged Styler and Time widget classes

---
+++ trunk/core/src/org/vast/stt/gui/widgets/time/TimeSpinner.java
@@ -0,0 +1,300 @@
+package org.vast.stt.gui.widgets.time;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.KeyListener;
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+import org.eclipse.swt.events.TraverseEvent;
+import org.eclipse.swt.events.TraverseListener;
+import org.eclipse.swt.events.VerifyEvent;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * <p><b>Title:</b><br/>
+ * TimeSpinner
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ * GUI for holding a Text widget and two arrow buttons to allow time step/bias/lead/lag
+ * selection.  Arrow buttons may be pressed and held to simulate spinner behavior.  
+ * </p>
+ *
+ * <p>Copyright (c) 2005</p>
+ * @author Tony Cook
+ * @date Dec 19, 2005
+ * @version 1.0
+ */
+public class TimeSpinner implements TraverseListener, //FocusListener,SelectionListener,  
+									MouseListener, KeyListener, DisposeListener //, VerifyListener
+{
+	Group mainGroup;
+	StyledText text;
+	Font entryFont;
+	Button upBtn, downBtn;
+	SpinnerModel tsModel;
+	boolean btnDown = false;  //  Used to indicate user is pressing and holding a spinner button
+	
+	protected TimeSpinner(){
+		//  Added so that CurrentTimeSpinner can extend this class
+		initFont();
+	}
+	
+	public TimeSpinner(Composite parent, String label) {
+		this();
+		tsModel = new TimeSpinnerModel("YYYY DDD HH:mm:SS");
+		initGui(parent, label);
+		//  Should really generate initial data from formatStr, but I don't 
+		//  think it will use this anyway.  In practice, setValue(double) will
+		//  be called at initialization
+		text.setText("0000 000 00:00:00");
+		text.setCaretOffset(13);
+		tsModel.selectField(text);
+	}
+	
+	public void setLayoutData(GridData gridData){
+		mainGroup.setLayoutData(gridData);
+	}
+	
+	protected void initFont(){
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		entryFont = new Font(display, new FontData("arial", 9, SWT.NORMAL));
+	}
+	
+	protected void initGui(Composite parent, String label){
+		//  Create main group for text and Buttons
+		mainGroup = new Group(parent, SWT.SHADOW_NONE);
+		mainGroup.setText(label);
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 2;
+		gridLayout.marginTop = -10;
+		gridLayout.marginBottom = -2;
+		gridLayout.marginRight = -3;
+		mainGroup.setLayout(gridLayout);
+		mainGroup.addDisposeListener(this);
+		//  Text widget		
+		text = new StyledText(mainGroup, SWT.RIGHT | SWT.BORDER | SWT.READ_ONLY);
+		//text = new Text(mainGroup, SWT.RIGHT | SWT.BORDER);
+		GridData gridData = new GridData();
+		//gridData.verticalAlignment = SWT.CENTER;
+		//gridData.heightHint = 18;
+		text.setLayoutData(gridData);
+		text.setFont(entryFont);
+		
+		//text.addSelectionListener(this);
+		//text.addModifyListener(this);
+		//text.addVerifyListener(this);
+		//text.addFocusListener(this);
+		text.addTraverseListener(this);
+		text.addMouseListener(this);
+		text.addKeyListener(this);
+
+		// SpinnerGroup
+		Composite spinnerGroup = new Composite(mainGroup, SWT.SHADOW_NONE);
+		gridLayout = new GridLayout();
+		gridLayout.numColumns = 1;
+		gridLayout.verticalSpacing = 2;
+		gridLayout.marginWidth = 1;
+		gridLayout.marginTop = -2;
+		gridLayout.marginBottom = -5;
+		spinnerGroup.setLayout(gridLayout);
+		gridData = new GridData();
+		gridData.verticalAlignment = SWT.CENTER;
+		spinnerGroup.setLayoutData(gridData);
+		//  Spinner buttons
+		upBtn = new Button(spinnerGroup, SWT.ARROW | SWT.UP);
+		gridData = new GridData();
+		gridData.heightHint = 15;
+    	upBtn.setLayoutData(gridData);
+    	upBtn.addMouseListener(this);
+		downBtn = new Button(spinnerGroup, SWT.ARROW | SWT.DOWN);
+		gridData = new GridData();
+		gridData.heightHint = 15;
+		downBtn.setLayoutData(gridData);
+    	downBtn.addMouseListener(this);
+	}
+
+	private void timeUp(){
+		int caretPos = text.getCaretOffset();
+		tsModel.increment();
+		text.setText(tsModel.toString());
+		text.setCaretOffset(caretPos);
+		tsModel.selectField(text);
+	}
+	
+	private void timeDown(){
+		int caretPos = text.getCaretOffset();
+		tsModel.decrement();
+		text.setText(tsModel.toString());
+		text.setCaretOffset(caretPos);
+		tsModel.selectField(text);
+	}
+
+	Runnable spinUpThread = new Runnable(){
+		public void run(){
+			timeUp();
+		}
+	};
+	
+	Runnable spinDownThread = new Runnable(){
+		public void run(){
+			timeDown();
+		}
+	};
+
+	private void startSpinUpThread(){
+		if(btnDown)
+			return;
+		btnDown = true;
+		timeUp();
+		Runnable spinThread = new Runnable(){
+			public void run(){
+				try {		
+					Thread.sleep(300l);
+					while(btnDown){
+							Thread.sleep(30l);
+							if(!text.isDisposed()) {
+								text.getDisplay().asyncExec(spinUpThread);
+							}
+					}	
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				//System.err.println("");
+			}
+			
+		};
+		Thread thread = new Thread(spinThread);
+		thread.start();
+	}
+
+	private void startSpinDownThread(){
+		if(btnDown)
+			return;
+		btnDown = true;
+		timeDown();
+		Runnable spinThread = new Runnable(){
+			public void run(){
+				try {		
+					Thread.sleep(500l);  //  make this less sensitive (mouseEvents seem to be getting queued here)
+					while(btnDown){
+							Thread.sleep(10l);
+							if(!text.isDisposed()) {
+								text.getDisplay().asyncExec(spinDownThread);
+							}
+					}	
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				//System.err.println("");
+			}
+			
+		};
+		Thread thread = new Thread(spinThread);
+		thread.start();
+	}
+
+	private void stopSpinThread(){
+		btnDown = false;
+		text.setFocus();
+		//System.err.println("Value is " + tsModel.getValue());
+		//System.err.println("stopUpThread");
+	}
+
+	public void setEnabled(boolean b){
+		mainGroup.setEnabled(b);
+	}
+
+	public void setValue(double value){
+		tsModel.setValue(new Double(value));
+		text.setText(tsModel.toString());
+		tsModel.selectField(text);
+	}
+	
+	public double getValue(){
+		Double val = (Double)tsModel.getValue();
+		return val.doubleValue();
+	}
+	
+	/**
+	 * Override behavior of up-down-left-right arrow keys
+	 */
+	public void keyTraversed(TraverseEvent e) {
+		if(e.keyCode == SWT.ARROW_DOWN) {
+			timeDown();
+		} else if (e.keyCode == SWT.ARROW_UP) {
+			timeUp();
+		} else {
+			tsModel.selectField(text);
+		}
+	}
+	
+	public void mouseDoubleClick(MouseEvent e) {
+		//  do nothing...
+	}
+
+	public void mouseDown(MouseEvent e) {
+		if(e.widget == upBtn) {
+			startSpinUpThread();
+		} else if(e.widget == downBtn) {
+			startSpinDownThread();
+		}
+	}
+
+
+	public void mouseUp(MouseEvent e) {
+		btnDown = false;
+		if(e.widget == upBtn || e.widget == downBtn) {
+			stopSpinThread();
+		} else  // e.widget == text
+			tsModel.selectField(text);
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if((e.keyCode<48 || e.keyCode > 57)) { // && e.keyCode!=0) {
+			//System.err.println("NOT A NUMBER");
+			//e.doit = false;
+			return;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * verifyText - only allow numeric entry for this
+	 */
+	public void verifyText(VerifyEvent e) {
+		System.err.println("** Verified Text is " + e);
+		e.doit = false;
+		if((e.keyCode<48 || e.keyCode > 57) && e.keyCode!=0) {
+			System.err.println("NOT A NUMBER");
+			e.doit = false;
+		}
+	}
+
+	/**
+	 *  Added dispose listener to dispose Font I use for the text fields.  If we 
+	 *  implement a FontRegistry, I can get rid of this.  TC 
+	 * @param e
+	 */
+	public void widgetDisposed(DisposeEvent e) {
+		entryFont.dispose();
+	}
+	
+}
---
+++ trunk/core/src/org/vast/stt/gui/widgets/time/TimeSpinnerModel.java
@@ -0,0 +1,318 @@
+package org.vast.stt.gui.widgets.time;
+
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
+
+import org.eclipse.swt.custom.StyleRange;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.PlatformUI;
+/**
+ * <p><b>Title:</b><br/>
+ * TimeSpinnerModel
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ * The Model for controlling the behavior of the timeSpinner widget, mainly through 
+ * the increment() and decrement() methods.  The model also highlights the currently 
+ * selected field in the selectField() method.  It should also work for fraction of seconds
+ * by passing in one or more 'F's in the format string, but that has not been tested.
+ * </p>
+ *
+ * <p>Copyright (c) 2005</p>
+ * @author Tony Cook
+ * @date Dec 20, 2005
+ * @version 1.0
+ */
+public class TimeSpinnerModel implements SpinnerModel{
+	
+	String formatStr;
+	// position of field in arrays start and len
+    protected int YEAR = -1 , MONTH = -1, DAY = -1, HOUR = -1, MIN = -1, SEC = -1, FSEC = -1;
+    int [] start, len; // position of fields in Str
+    int yearDigits, dayDigits, fsecDigits;
+    protected int years, months, days, hours, minutes, seconds, fseconds;
+    int maxYears, maxDays, maxFseconds;  //  editor supplies these
+    boolean hasFseconds = false;  //  is fraction of seconds present?
+    boolean rollEnabled = true;
+    boolean allowNegative = false;
+    int currentField = 0;
+    //TODO: sub these constants out for something like the old Units class
+	static final int SECONDS_PER_MINUTE = 60;
+	static final int SECONDS_PER_HOUR = 3600;
+	static final int SECONDS_PER_DAY = 24 * SECONDS_PER_HOUR;
+	static final int SECONDS_PER_YEAR = 365 * SECONDS_PER_DAY; 
+    
+	public TimeSpinnerModel(String formatStr){
+		this.formatStr = formatStr;
+		loadFieldPositionArrays(formatStr);
+	}
+	
+    public void loadFieldPositionArrays(String formatStr){
+        String token;
+        StringTokenizer parser = new StringTokenizer(formatStr," :./,", true);
+        int numFields = parser.countTokens();
+        start = new int[numFields];
+        len = new int[numFields];
+        int index = 0;
+        int where = 0;
+
+        try {
+            while (parser.hasMoreTokens()){
+                token = parser.nextToken();
+                switch(token.charAt(0)) {
+                    case 'Y': case 'y':
+                        YEAR = index;
+                        yearDigits = token.length();
+                        maxYears = (int)(Math.pow(10.0, (double)yearDigits) - 1);                        
+                        break;
+                    case 'M':
+                        MONTH = index;
+                        break;
+                    case 'D': case 'd':
+                        DAY = index;
+                        dayDigits = token.length();
+                        maxDays = (int)(Math.pow(10.0, (double)dayDigits) - 1);
+                        break;
+                    case 'H': case 'h':
+                        HOUR = index;
+                        break;
+                    case 'm':
+                        MIN = index;
+                        break;
+                    case 'S': case 's':
+                        SEC = index;
+                        break;
+                    case 'F': case 'f':
+                        FSEC = index;  // fratction of seconds
+                        fsecDigits = token.length();
+                        maxFseconds = (int)(Math.pow(10.0, (double)fsecDigits) - 1);
+                        break;
+                    case ' ':
+                    case ':':
+                    case '.':
+                    case '/':
+                    case ',':
+                        where++;  numFields--;
+                        continue;
+                    default:
+                        System.err.println("TimeStepSpinnerEditor:  Format invalid: " + formatStr);
+                        return;
+                }
+
+                len[index] = token.length();
+                start[index] = where;
+                where = start[index]+len[index];
+
+                index++;
+            }
+
+        }catch (NoSuchElementException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+	
+	private void stepCurrentField(int step){
+		if(currentField == YEAR){
+            years += step;
+            if(years<0)  years = maxYears;
+            if(years>maxYears)  years = 0;
+		} else if(currentField == MONTH){
+            ;//changeDay(step);
+		} else if(currentField == DAY){
+            changeDay(step);
+		} else if(currentField == HOUR){
+            changeHour(step);
+        } else if(currentField == MIN) {
+        	changeMinute(step);
+		} else if(currentField == SEC) {
+			changeSecond(step);
+		} else if(currentField == FSEC) {
+			changeFsecond(step);
+		} else 
+			System.err.println(" *** currentField unknown.  Blimey!!!!");
+			
+	}
+
+	public void increment(){
+		stepCurrentField(1);
+		//return toString();
+	}
+    
+	public void decrement(){
+		stepCurrentField(-1);
+		//return toString();
+	}
+	
+	/** 
+	 * TODO:  modify to support different formatStrings
+	 */
+    public String toString(){
+    	StringBuffer buff = new StringBuffer(formatStr.length());
+    	buff.append(zeroPad("" + years, yearDigits) + " ");
+    	buff.append(zeroPad("" + days, dayDigits) + " ");
+    	buff.append(zeroPad("" + hours,2) + ":");
+    	buff.append(zeroPad("" + minutes,2) + ":");
+    	buff.append(zeroPad("" + seconds,2));
+    			
+    	return buff.toString();
+    }
+	
+    private String zeroPad(String str, int desiredLength){
+        int count = desiredLength - str.length();
+        if(count <= 0)  // if < 0, we have a problem actually
+            return str;
+        char [] zeros = new char[count];
+        for(int i=0;i<zeros.length;i++){
+            zeros[i] = '0';
+        }
+        String zeroStr = new String(zeros);
+        return zeroStr + str;
+    }
+
+
+    //  NOTE:  step should be either 1 or -1
+    protected void changeDay(int step){
+        days+=step;
+        if(days > maxDays)  {
+            days = 0;
+            //  No year rolling by design for now.  TC 1/25/05
+        } else if (days < 0)
+            days = maxDays;
+    }
+
+    //  NOTE:  step should be either 1 or -1
+    protected void changeHour(int step){
+        hours+=step;
+        if(hours > 23)  {
+            hours = 0;
+            if(rollEnabled)
+                changeDay(step);
+        } else if (hours < 0) {
+            hours = 23;
+            if(rollEnabled)
+                changeDay(step);
+        }
+    }
+
+    //  NOTE:  step should be either 1 or -1
+    protected void changeMinute(int step){
+        minutes+=step;
+        if(minutes > 59)  {
+            minutes = 0;
+            if(rollEnabled)
+                changeHour(step);
+        } else if (minutes < 0) {
+            minutes = 59;
+            if(rollEnabled)
+                changeHour(step);
+        }
+    }
+
+    //  NOTE:  step should be either 1 or -1
+    protected void changeSecond(int step){
+        seconds+=step;
+        if(seconds > 59)  {
+            seconds = 0;
+            if(rollEnabled)
+                changeMinute(step);
+        } else if (seconds < 0) {
+            seconds = 59;
+            if(rollEnabled)
+                changeMinute(step);
+        }
+    }
+
+    //  NOTE:  step should be either 1 or -1
+    protected void changeFsecond(int step){
+        fseconds+=step;
+        if(fseconds > maxFseconds)  {
+            fseconds = 0;
+            if(rollEnabled)
+                changeSecond(step);
+        } else if (fseconds < 0) {
+            fseconds = maxFseconds;
+            if(rollEnabled)
+                changeSecond(step);
+        }
+    }
+
+    public void selectField(StyledText text){
+		int caretPos = text.getCaretOffset();
+		//  Force single characer selection
+		text.setSelection(caretPos,caretPos);
+        //System.err.println("** selField: text, caretPos = "  +  text.getText(0,3)+ ", " + caretPos);
+        for(int i=0; i<start.length; i++){
+            if(caretPos <= (start[i] + len[i])) {
+            	//if(i!=currentField) {
+            		//  Set old field to default fg/bg
+                    Display display = PlatformUI.getWorkbench().getDisplay();
+            		StyleRange range = new StyleRange(start[currentField],len[currentField],
+            								text.getForeground(), text.getBackground());
+            		text.setStyleRange(range);
+            		//  Set new field to hilite colors
+                    Color fgColor = new Color(display, 228,228,228);
+            	    Color bgColor = new Color(display,0,0,128);            	    
+            		range = new StyleRange(start[i],len[i],fgColor, bgColor);
+            		currentField = i;
+            		text.setStyleRange(range);
+            	//}
+            	return;
+            }
+            	
+        }
+        System.err.println("TimeSpinnerModel.selectField():  field pos not found");
+        currentField = -1;
+	}
+
+    public Object getValue(){
+        double timeStep = years * SECONDS_PER_YEAR;  // no leap year here.
+        timeStep += days * SECONDS_PER_DAY;
+        timeStep += hours * SECONDS_PER_HOUR;
+        timeStep += minutes * SECONDS_PER_MINUTE;
+        timeStep += seconds;
+        if(hasFseconds)
+            timeStep += fseconds/maxFseconds;
+
+        return new Double(timeStep);
+    }
+    
+    /**
+     * Sets the spinner value based on number of seconds
+     * @param sec - seconds since 1Jan1970
+     */
+    public void setValue(Object secondsObj){
+    	double sec = ((Double)secondsObj).doubleValue();
+        //  clear all fields
+        years = days = hours = minutes = seconds = fseconds = 0;
+        int isec = (int)sec;
+        if (hasFseconds) {
+            double frac = sec - (double)isec;
+            String fsStr = "" + maxFseconds;
+            int numDigits = fsStr.length();
+            fseconds = (int)(Math.pow(frac, numDigits));
+        }
+        if(isec >= SECONDS_PER_YEAR) {
+            years = isec / (int)SECONDS_PER_YEAR;
+            isec = isec % (int)SECONDS_PER_YEAR;
+        }
+        if(isec >= SECONDS_PER_DAY) {
+            days = isec / (int)SECONDS_PER_DAY;
+            isec = isec % (int)SECONDS_PER_DAY;
+        }
+        if(isec >= SECONDS_PER_HOUR) {
+            hours = isec / (int)SECONDS_PER_HOUR;
+            isec = isec % (int)SECONDS_PER_HOUR;
+        }
+        if(isec >= SECONDS_PER_MINUTE) {
+            minutes = isec / 60;
+            isec = isec % 60;
+        }
+        seconds = isec;
+    }
+
+    
+}
