7adad84aa9f76ba93abbaa828e691bdc4246aa69
robin <robin@813d399c-aa55-0410-8b7d-c12975737b24>
2006-01-25 02:22:37 +0000

    Updated PointStyler

--- trunk/core/src/org/vast/stt/style/PointStyler.java
+++ trunk/core/src/org/vast/stt/style/PointStyler.java
@@ -13,9 +13,13 @@
 
 package org.vast.stt.style;
 
+import org.vast.ows.sld.Color;
+import org.vast.ows.sld.GraphicMark;
+import org.vast.ows.sld.GraphicSource;
 import org.vast.ows.sld.PointSymbolizer;
+import org.vast.ows.sld.ScalarParameter;
 import org.vast.ows.sld.Symbolizer;
-import org.vast.stt.util.SpatialExtent;
+import org.vast.stt.data.DataNode;
 
 
 /**
@@ -37,6 +41,7 @@ public class PointStyler extends AbstractStyler
 {
 	protected PointSymbolizer symbolizer;
 	protected PointGraphic point;
+    protected int[][] xIndices, yIndices, zIndices;
 	
 	
 	public PointStyler()
@@ -49,45 +54,96 @@ public class PointStyler extends AbstractStyler
 	
 	public PointGraphic getPoint(int i)
 	{
-		point.x = node.getComponent(i).getComponent(2).getData().getDoubleValue();
-		point.y = node.getComponent(i).getComponent(1).getData().getDoubleValue();
-		point.z = node.getComponent(i).getComponent(3).getData().getDoubleValue() / 1e6;
-		point.g = 1.0f;
-		return point;
+        point.x = this.getComponent(node, xIndices, i).getData().getDoubleValue();
+        point.y = this.getComponent(node, yIndices, i).getData().getDoubleValue();
+        point.z = this.getComponent(node, zIndices, i).getData().getDoubleValue() / 1e6;
+        return point;
 	}
 	
 	
 	public int getPointCount()
 	{
-		return node.getComponentCount();
-	}
-	
-	
-	public SpatialExtent getBoundingBox()
-	{
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public double[] getCenterPoint()
-	{
-		// TODO Auto-generated method stub
-		return null;
+		return this.getComponentCount(xIndices);
 	}
 
 
 	public void updateBoundingBox()
 	{
 		// TODO Auto-generated method stub
-		
+	    // compute bounding box by scanning max and min geometry values
 	}
 
 
 	public void updateDataMappings()
 	{
-		// TODO Auto-generated method stub
-        node = dataProvider.getDataNode();
+        ScalarParameter param;
+        String propertyName;
+        Object value;
+        
+        // generate indexing rules for geometry components
+        DataNode newNode = dataProvider.getDataNode();
+        //if (newNode == node)
+        //    return;
+        
+        node = newNode;
+        
+        // geometry X
+        param = this.symbolizer.getGeometry().getX();
+        if (param != null)
+        {
+            propertyName = param.getPropertyName();            
+            if (propertyName != null)
+                xIndices = computeIndices(null, propertyName);
+        }
+        
+        //geometry Y
+        param = this.symbolizer.getGeometry().getY();
+        if (param != null)
+        {
+            propertyName = param.getPropertyName();            
+            if (propertyName != null)
+                yIndices = computeIndices(null, propertyName);
+        }
+        
+        // geometry Z
+        param = this.symbolizer.getGeometry().getZ();
+        if (param != null)
+        {
+            propertyName = param.getPropertyName();            
+            if (propertyName != null)
+                zIndices = computeIndices(null, propertyName);
+        }
+                
+        // point color
+        GraphicSource glyph = this.symbolizer.getGraphic().getGlyphs().get(0);
+        if (glyph instanceof GraphicMark)
+        {
+            param = ((GraphicMark)glyph).getFill().getColor();
+            if (param != null)
+            {
+                value = param.getConstantValue();
+                if (value != null)
+                {
+                    Color lineColor = (Color)value;
+                    point.r = lineColor.getRed();
+                    point.g = lineColor.getGreen();
+                    point.b = lineColor.getBlue();
+                    point.a = lineColor.getAlpha();
+                }
+            }
+        }        
+        
+        // point size
+        param = this.symbolizer.getGraphic().getSize();
+        if (param != null)
+        {
+            value = param.getConstantValue();
+            if (value != null)
+            {
+                Float pointSize = (Float)value;
+                point.size = pointSize.intValue();
+            }
+        }
 	}
 	
 	
