7c49b5098c2c32db768066f114bf7a5b340ece3c
tcook <tcook@813d399c-aa55-0410-8b7d-c12975737b24>
2006-01-27 02:15:17 +0000

    Abstract superclasses for "double table" widget used by StyleView and DataProviderView

---
+++ core/src/org/vast/stt/gui/widgets/CheckOptionTable.java
@@ -0,0 +1,191 @@
+package org.vast.stt.gui.widgets;
+
+import org.eclipse.jface.viewers.CheckboxTableViewer;
+import org.eclipse.jface.viewers.ICheckStateListener;
+import org.eclipse.jface.viewers.IContentProvider;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.events.SelectionListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Group;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.ui.PlatformUI;
+import org.vast.stt.scene.DataItem;
+
+/**
+ * <p><b>Title:</b><br/>
+ * CheckOptionTable 
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ *	Abstract "double table" widget, with top panel being checkboxTable, 
+ *  and bottom panel being a list of OptionControls.  Not sure what I 
+ *  should call it.     
+ *  
+ *  Concrete subclasses include StyleWidget and DataProviderWidget
+ * </p>
+ *
+ * <p>Copyright (c) 2006</p>
+ * @author Tony Cook
+ * @date Jan 26, 2006
+ * @version 1.0
+ * 
+ */
+
+abstract public class CheckOptionTable {
+	DataItem dataItem;
+	Table table;
+	Group mainGroup;
+	Composite optionsComp;
+	ScrolledComposite stylesSC;
+	protected CheckboxTableViewer checkboxTableViewer;
+	protected OptionChooser optionChooser;
+	
+	boolean allowAddRemove = true;
+	protected Button deleteButton;
+	protected Button addButton;
+	protected Button enabledButton;
+	protected Button advancedButton;
+	
+	abstract public OptionChooser createOptionChooser(Composite parent);
+	
+	public void setDataItem(DataItem item){
+		dataItem = item;
+		mainGroup.setText(item.getName());
+		enabledButton.setData(dataItem);
+		enabledButton.setSelection(dataItem.isEnabled());
+		System.err.println("SW.setDI(): " +  dataItem.getName());
+	}
+
+	public void init(Composite parent) {
+		//  Check for DataItem, OptionChooser
+		
+		final ScrolledComposite mainSC = new ScrolledComposite(parent,
+				SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		
+		mainSC.setExpandVertical(true);
+		mainSC.setExpandHorizontal(true);
+		//  START HERE:  Try this with OptSCroller
+		mainSC.setLayoutData(new GridData(GridData.FILL, GridData.FILL, true, true, 1, 1));
+
+		mainGroup = new Group(mainSC, SWT.NONE);
+		mainGroup.setText("Item Name");
+		final GridLayout gridLayout = new GridLayout(6, true);
+		//gridLayout.numColumns = 6;
+		mainGroup.setLayout(gridLayout);
+		mainGroup.setLocation(0, 0);
+
+        // Enabled Button
+		enabledButton = new Button(mainGroup, SWT.CHECK);
+		enabledButton.setData(dataItem);
+        final GridData gridData = new GridData();
+        gridData.verticalIndent = 7;
+        gridData.horizontalAlignment = GridData.BEGINNING;
+        gridData.horizontalSpan = 6;
+        enabledButton.setLayoutData(gridData);
+        enabledButton.setText("enabled");
+		
+        // Styles Label
+        final Label stylesLabel = new Label(mainGroup, SWT.NONE);
+        final GridData gridData_3 = new GridData();
+        gridData_3.horizontalAlignment = GridData.BEGINNING;
+        gridData_3.grabExcessHorizontalSpace = true;
+        gridData_3.horizontalSpan = 4;
+        stylesLabel.setLayoutData(gridData_3);
+        stylesLabel.setText("Styles:");
+
+        // Add Style Button
+        if(allowAddRemove) {
+			addButton = new Button(mainGroup, SWT.NONE);
+			final GridData gridData_4 = new GridData();
+	        gridData_4.horizontalAlignment = GridData.FILL;
+	        gridData_4.grabExcessHorizontalSpace = true;
+	        gridData_4.horizontalSpan = 1;
+	        addButton.setLayoutData(gridData_4);
+	        addButton.setText("+");        
+					
+			deleteButton = new Button(mainGroup, SWT.NONE);
+			final GridData gridData_5 = new GridData();
+	        gridData_5.horizontalAlignment = GridData.FILL;
+	        gridData_5.grabExcessHorizontalSpace = true;
+	        gridData_5.horizontalSpan = 1;
+	        deleteButton.setLayoutData(gridData_5);
+	        deleteButton.setText("-");
+        }
+		
+		//  CheckboxTableViewer for styles
+		checkboxTableViewer = CheckboxTableViewer.newCheckList(mainGroup, SWT.BORDER | SWT.SINGLE); 
+		table = checkboxTableViewer.getTable();
+		table.setBackground(PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_WHITE));
+		final GridData tableGd = new GridData(GridData.FILL, GridData.FILL, true, true);
+
+		tableGd.minimumHeight = 75;
+		//tableGd.widthHint = 125;
+		tableGd.heightHint = 55;
+        tableGd.horizontalSpan = 6;
+        tableGd.grabExcessVerticalSpace = true;
+		table.setLayoutData(tableGd);
+
+        // Options Label
+		final Label optLabel = new Label(mainGroup, SWT.NONE);
+		final GridData gridData_6 = new GridData();
+        gridData_6.horizontalSpan = 6;
+        gridData_6.horizontalAlignment = GridData.BEGINNING;
+		optLabel.setLayoutData(gridData_6);
+        optLabel.setText("Options:");
+
+		// OptionsChooser
+		optionChooser = createOptionChooser(mainGroup);
+		
+        // Advanced Button
+		advancedButton = new Button(mainGroup, SWT.NONE);		
+		final GridData gridData_7 = new GridData();
+		gridData_7.horizontalSpan = 6;
+		gridData_7.horizontalAlignment = GridData.END;
+		advancedButton.setLayoutData(gridData_7);
+		advancedButton.setText("Advanced...");
+
+		mainSC.setContent(mainGroup);
+		mainSC.setMinSize(mainGroup.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+	}
+	
+	public void addSelectionListener(SelectionListener sl){
+		addButton.addSelectionListener(sl);
+		deleteButton.addSelectionListener(sl);
+		enabledButton.addSelectionListener(sl);
+		advancedButton.addSelectionListener(sl);
+	}
+	
+	public void removeSelectionListener(SelectionListener sl){
+		addButton.removeSelectionListener(sl);
+		deleteButton.removeSelectionListener(sl);
+		enabledButton.removeSelectionListener(sl);
+		advancedButton.removeSelectionListener(sl);
+	}
+	
+	public void addCheckboxTableListener(ICheckStateListener checkStateListener, 
+											 ISelectionChangedListener selectionChangedListener){
+		checkboxTableViewer.addCheckStateListener(checkStateListener);
+		checkboxTableViewer.addSelectionChangedListener(selectionChangedListener);
+	}
+	
+	public void removeCheckboxTableListener(ICheckStateListener checkStateListener, 
+		 ISelectionChangedListener selectionChangedListener){
+		checkboxTableViewer.removeCheckStateListener(checkStateListener);
+		checkboxTableViewer.removeSelectionChangedListener(selectionChangedListener);
+	}
+
+	public void setcheckboxTableContentProvider(IContentProvider tableContentProv){
+		checkboxTableViewer.setContentProvider(tableContentProv);
+	}
+
+	public void setcheckboxTableLabelProvider(ILabelProvider tableLabelProv){
+		checkboxTableViewer.setLabelProvider(tableLabelProv);
+	}
+}
---
+++ core/src/org/vast/stt/gui/widgets/OptionChooser.java
@@ -0,0 +1,54 @@
+package org.vast.stt.gui.widgets;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.PlatformUI;
+
+public abstract class OptionChooser {
+	protected Composite optComp;
+	protected ScrolledComposite optScr;
+	
+	public OptionChooser(Composite parent){
+		init(parent);
+	}
+
+	public void init(Composite parent){
+		optScr = new ScrolledComposite(parent, SWT.V_SCROLL | SWT.H_SCROLL | SWT.BORDER);
+		optScr.setExpandHorizontal(true);
+		optScr.setExpandVertical(true);
+		optComp = new Composite(optScr, SWT.NONE);
+		optScr.setContent(optComp);
+
+		GridData scrollerGD = new GridData();
+        scrollerGD.horizontalSpan = 6;
+        scrollerGD.horizontalAlignment = GridData.FILL;
+        scrollerGD.verticalAlignment = GridData.FILL;
+        scrollerGD.grabExcessHorizontalSpace = true;
+        scrollerGD.grabExcessVerticalSpace = true;
+        //scrollerGD.heightHint = 80;
+        scrollerGD.minimumHeight = 80;
+		optScr.setLayoutData(scrollerGD);
+		
+		GridLayout optLayout = new GridLayout(1, false);
+		optComp.setLayout(optLayout);
+		optComp.setBackground(PlatformUI.getWorkbench().getDisplay().getSystemColor(SWT.COLOR_WHITE));
+	}
+
+	abstract public void buildControls(Object inputElement);
+	
+	//  There's no way to remove a Control from a Composite (that I have found)
+	//  Therefore, just dispose and null them, and recreate as needed.  TC
+	public void removeOldControls(){
+		Control [] controls = optComp.getChildren();
+		for(int i=0; i<controls.length; i++){
+			controls[i].dispose();
+			controls[i] = null;
+		}
+	}
+
+
+}
