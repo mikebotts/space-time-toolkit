7ba68fedaf6af1001fa95c637346b207da742654
tcook <tcook@813d399c-aa55-0410-8b7d-c12975737b24>
2006-01-27 02:16:13 +0000

    Extracted superclasses for StyleWidget so they could be reused by DataProviderWidget

--- core/src/org/vast/stt/gui/widgets/styler/PointOptionController.java
+++ core/src/org/vast/stt/gui/widgets/styler/PointOptionController.java
@@ -1,5 +1,7 @@
 package org.vast.stt.gui.widgets.styler;
 
+import java.util.List;
+
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Button;
@@ -8,6 +10,9 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Spinner;
 import org.vast.ows.sld.Color;
+import org.vast.ows.sld.Graphic;
+import org.vast.ows.sld.GraphicMark;
+import org.vast.ows.sld.GraphicSource;
 import org.vast.ows.sld.ScalarParameter;
 import org.vast.stt.style.PointStyler;
 
@@ -35,24 +40,13 @@ public class PointOptionController implements OptionController{
 		optionControl = new OptionControl[2];
 		optionControl[0] = new OptionControl(parent);
 		sizeSpinner = optionControl[0].createSpinner("Point Size:", 1,10);
-
+		sizeSpinner.setSelection((int)getPointSize());
+		
 		optionControl[1] = new OptionControl(parent);
-//		ScalarParameter colorSP = styler.getSymbolizer().get
-//		Color sldColor;
-//		if(colorSP != null) 
-//			sldColor = (Color)colorSP.getConstantValue();
-//		else 
-//			sldColor = new Color(1.0f, 0.0f, 0.0f, 1.0f);
-		//colorButton = optionControl[1].createColorButton("Point Color:", sldColor);
-		colorButton = optionControl[1].createButton("Point Color:", "...");
+		colorButton = optionControl[1].createColorButton("Point Color:", getPointColor());
 		colorButton.addSelectionListener(this);
 	}
 	
-//	public void turnOnControls(){
-//		for(int i=0; i<optionControl.length; i++)
-//			optionControl[i].setVisible(true);
-//	}
-
 	public void widgetDefaultSelected(SelectionEvent e) {
 		// TODO Auto-generated method stub
 		
@@ -62,26 +56,69 @@ public class PointOptionController implements OptionController{
 		Control control = (Control)e.getSource();
 		//  uncomment when Stylers are working
 		if(control == sizeSpinner) {
-	//		Graphic graphic = styler.getSymbolizer().getGraphic();
-	//		ScalarParameter size = new ScalarParameter();
-	//		size.setConstantValue(new Integer(spinner.getSelection()));
-	//		graphic.setSize(new ScalarParameter());
+			float size = (float)sizeSpinner.getSelection();
+			setPointSize(size);
+			styler.updateDataMappings();
 		} else if(control == colorButton) {
 			ColorDialog colorChooser = new ColorDialog(colorButton.getShell());
 			RGB rgb = colorChooser.open();
-			//  uncomment when Stylers are working
-//			PointStyler styler = (PointStyler)button.getData();
-//			Graphic graphic = styler.getSymbolizer().getGraphic();
-//			List graphicSourceList = graphic.getImages();
-//			if(graphicSourceList == null)  return;
-//			GraphicSource graphicSource = (GraphicSource)graphicSourceList.get(0);
-//			if(graphicSource instanceof GraphicMark) {
-//				GraphicMark gm = (GraphicMark)graphicSource;
-//				ScalarParameter newColor = new ScalarParameter();
-//				newColor.setConstantValue(rgb);
-//				gm.getFill().setColor(newColor);
-//			}
+			setPointColor(rgb);
+			styler.updateDataMappings();
 		}
 	}
 
+	private void setPointSize(float f){
+		Graphic graphic = styler.getSymbolizer().getGraphic();
+		ScalarParameter size = new ScalarParameter();
+		size.setConstantValue(f);
+		graphic.setSize(size);
+	}
+
+	/**
+	 * Convenience method to set line color
+	 * @param swtRgb
+	 */
+	private void setPointColor(RGB swtRgb){
+		Color color = new Color(swtRgb.red, swtRgb.green, swtRgb.blue, 255);
+		optionControl[1].setColorLabelColor(color);
+		Graphic graphic = styler.getSymbolizer().getGraphic();
+		List graphicSourceList = graphic.getGlyphs();
+		if(graphicSourceList == null)  return;
+		GraphicSource graphicSource = (GraphicSource)graphicSourceList.get(0);
+		if(graphicSource instanceof GraphicMark) {
+			GraphicMark gm = (GraphicMark)graphicSource;
+			ScalarParameter newColor = new ScalarParameter();
+			newColor.setConstantValue(color);
+			gm.getFill().setColor(newColor);
+		}
+	}
+	
+	public float getPointSize(){
+		Graphic graphic = styler.getSymbolizer().getGraphic();
+		ScalarParameter size = graphic.getSize();
+		if(size == null)
+			return 1.0f;
+		Object val = size.getConstantValue();
+		if(val == null)
+			return 1.0f;
+		return ((Float)val).floatValue();
+	}
+	
+	public org.vast.ows.sld.Color getPointColor(){
+		Graphic graphic = styler.getSymbolizer().getGraphic();
+		List graphicSourceList = graphic.getGlyphs();
+		//if(graphicSourceList == null)  return;
+		GraphicSource graphicSource = (GraphicSource)graphicSourceList.get(0);
+		if(graphicSource instanceof GraphicMark) {
+			GraphicMark gm = (GraphicMark)graphicSource;
+			ScalarParameter color = gm.getFill().getColor();
+			if(color == null)
+				return new Color(1.0f, 0.0f, 0.0f, 1.0f);
+			Object val = color.getConstantValue();
+			if(val == null)
+				return new Color(1.0f, 0.0f, 0.0f, 1.0f);
+			return (Color)val;
+		}
+		return new Color(1.0f, 0.0f, 0.0f, 1.0f);
+	}
 }
