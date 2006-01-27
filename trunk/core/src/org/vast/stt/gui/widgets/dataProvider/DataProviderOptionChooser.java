6e27ad490c66264e05cac3fe99d10060ba84774f
tcook <tcook@813d399c-aa55-0410-8b7d-c12975737b24>
2006-01-27 04:08:06 +0000

    Skeleton of new DataProvider widgets

---
+++ trunk/core/src/org/vast/stt/gui/widgets/dataProvider/DataProviderOptionChooser.java
@@ -0,0 +1,48 @@
+package org.vast.stt.gui.widgets.dataProvider;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.vast.stt.gui.widgets.OptionChooser;
+
+/**
+ * <p><b>Title:</b><br/>
+ * DataProviderOptionChooser
+ * </p>
+ *
+ * <p><b>Description:</b><br/>
+ *
+ * </p>
+ *
+ * <p>Copyright (c) 2006</p>
+ * @author Tony Cook
+ * @date Jan 26, 2006
+ * @version 1.0
+ * 
+ * TODO  add chooser/mapping widget when user selects + (add) style
+ * TODO  support advanced options      
+ */
+
+public class DataProviderOptionChooser extends OptionChooser {
+	
+	public DataProviderOptionChooser(Composite parent) {
+		super(parent);
+	}
+
+	public void buildControls(Object providerObj){
+		removeOldControls();
+
+//		if(styler instanceof PointStyler) {
+//			//if(pointOpts == null)
+//				PointOptionController pointOpts = new PointOptionController(optComp, (PointStyler)styler);
+//		} else if (styler instanceof LineStyler) {
+//			//if(lineOpts == null)
+//				LineOptionController lineOpts = new LineOptionController(optComp, (LineStyler)styler); 
+//		} else
+//			System.err.println("Styler not supported yet: " + styler);
+		
+		optComp.layout(true);		
+		optScr.setMinSize(optComp.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		optComp.redraw();
+	}	
+
+}
