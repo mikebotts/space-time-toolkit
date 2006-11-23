4a3adbfde4dbd8e3a5e5637371bf49aa77ecb3f8
robin <robin@813d399c-aa55-0410-8b7d-c12975737b24>
2006-11-23 19:45:20 +0000

    Added damping to LLA projection

--- core/src/org/vast/stt/project/world/Projection_LLA.java
+++ core/src/org/vast/stt/project/world/Projection_LLA.java
@@ -43,7 +43,7 @@ public class Projection_LLA implements Projection
     protected final static double RTD = 180 / Math.PI;
     
     protected double centerLongitude = 0.0;
-    protected double altitudeDamping = 5e-7;
+    protected double altitudeDamping = 1e-6;
     protected double xSav = Double.NaN;
     protected double ySav = Double.NaN;
     
