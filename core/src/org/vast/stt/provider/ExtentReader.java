b3e5e092ef9b5a3bcfe55b055bb9f7a8bab028af
tcook <tcook@813d399c-aa55-0410-8b7d-c12975737b24>
2007-07-02 21:45:36 +0000

    Removed Provider dependence from ExtentReader

--- core/src/org/vast/stt/provider/ExtentReader.java
+++ core/src/org/vast/stt/provider/ExtentReader.java
@@ -53,69 +53,60 @@ public class ExtentReader extends XMLReader
      * @param dom
      * @param spElt
      */
-    public void readSpatialExtent(DataProvider provider, DOMHelper dom, Element spElt)
+    public STTSpatialExtent readSpatialExtent(DOMHelper dom, Element spElt)
     {
-        // get provider spatial extent
-        STTSpatialExtent spatialExtent = provider.getSpatialExtent();
-        
-        if (spElt != null)
-        {
-            Element bboxElt = dom.getElement(spElt, "BoundingBox");
-            
-            if (bboxElt != null)
-            {
-                // try to get the bbox from the list
-                Object obj = findExistingObject(dom, bboxElt);
-                if (obj != null)
-                {
-                    provider.setSpatialExtent((STTSpatialExtent)obj);
-                    return;
-                }          
-            
-                // read bbox
-                String coordText = dom.getElementValue(bboxElt, "coordinates");
-                String [] coords = coordText.split(" |,");
-                 
-                double minX = Double.parseDouble(coords[0]);
-                double minY = Double.parseDouble(coords[1]);
-                double maxX = Double.parseDouble(coords[2]);
-                double maxY = Double.parseDouble(coords[3]);
-                 
-                spatialExtent.setMinX(minX);
-                spatialExtent.setMinY(minY);
-                spatialExtent.setMaxX(maxX);
-                spatialExtent.setMaxY(maxY);
-                 
-                // read tiling info
-                String tileDims = dom.getAttributeValue(spElt, "tiling");
-                if (tileDims != null)
-                {
-                    String[] dims = tileDims.split("x");
-                    
-                    int tileX = Integer.parseInt(dims[0]);
-                    int tileY = Integer.parseInt(dims[1]);
-                     
-                    spatialExtent.setXTiles(tileX);
-                    spatialExtent.setYTiles(tileY);
-                    spatialExtent.setTilingEnabled(true);
-                }
-                 
-                // store that in the hashtable
-                registerObjectID(dom, bboxElt, spatialExtent);
-            }
-            
-            // read autoUpdate element
-            if (dom.existAttribute(spElt, "@autoUpdate"))
-            {
-                String sceneId = dom.getAttributeValue(spElt, "@autoUpdate").substring(1);
-                WorldScene scene = (WorldScene)objectIds.get(sceneId);
-                SpatialExtentUpdater updater = new SceneBboxUpdater(scene);
-                spatialExtent.setUpdater(updater);
-                updater.setEnabled(true);
-            }
+        STTSpatialExtent spatialExtent = new STTSpatialExtent();
+        Element bboxElt = dom.getElement(spElt, "BoundingBox");
+
+        if (bboxElt != null) {
+	        // try to get the bbox from the list
+	        Object obj = findExistingObject(dom, bboxElt);
+	        if (obj != null)
+	            return (STTSpatialExtent)obj;
+	    
+	        // read bbox
+	        String coordText = dom.getElementValue(bboxElt, "coordinates");
+	        String [] coords = coordText.split(" |,");
+	         
+	        double minX = Double.parseDouble(coords[0]);
+	        double minY = Double.parseDouble(coords[1]);
+	        double maxX = Double.parseDouble(coords[2]);
+	        double maxY = Double.parseDouble(coords[3]);
+	         
+	        spatialExtent.setMinX(minX);
+	        spatialExtent.setMinY(minY);
+	        spatialExtent.setMaxX(maxX);
+	        spatialExtent.setMaxY(maxY);
+	         
+	        // read tiling info
+	        String tileDims = dom.getAttributeValue(spElt, "tiling");
+	        if (tileDims != null)
+	        {
+	            String[] dims = tileDims.split("x");
+	            
+	            int tileX = Integer.parseInt(dims[0]);
+	            int tileY = Integer.parseInt(dims[1]);
+	             
+	            spatialExtent.setXTiles(tileX);
+	            spatialExtent.setYTiles(tileY);
+	            spatialExtent.setTilingEnabled(true);
+	        }
+	         
+	        // store that in the hashtable
+	        registerObjectID(dom, bboxElt, spatialExtent);
         }
+        // read autoUpdate element
+        if (dom.existAttribute(spElt, "@autoUpdate"))
+        {
+            String sceneId = dom.getAttributeValue(spElt, "@autoUpdate").substring(1);
+            WorldScene scene = (WorldScene)objectIds.get(sceneId);
+            SpatialExtentUpdater updater = new SceneBboxUpdater(scene);
+            spatialExtent.setUpdater(updater);
+            updater.setEnabled(true);
+        }
+        
+        return spatialExtent;
     }
-    
     
     /**
      * Reads the provider time extent data
@@ -123,68 +114,73 @@ public class ExtentReader extends XMLReader
      * @param dom
      * @param spElt
      */
-    public void readTimeExtent(DataProvider provider, DOMHelper dom, Element timeElt)
+    public STTTimeExtent readTimeExtent(DOMHelper dom, Element timeElt)
     {
-        // get provider time extent
-        STTTimeExtent timeExtent = provider.getTimeExtent();
-        
-        if (timeElt != null)
+    	STTTimeExtent timeExtent = new STTTimeExtent();
+    	
+    	if (timeElt != null)
+    	{    		
+	        Element extentElt = dom.getElement(timeElt, "TimeExtent");
+	        String val = null;            
+	        
+	        try
+	        {    
+	            if (extentElt != null)
+	            {
+	                // try to get the time extent from the list
+	                Object obj = findExistingObject(dom, extentElt);
+	                if (obj != null)
+	                {
+	                    return (STTTimeExtent)obj;
+	                }
+	                
+	                // read base time
+	                val = dom.getElementValue(extentElt, "baseTime");
+	                if (val.equalsIgnoreCase("now"))
+	                    timeExtent.setBaseAtNow(true);
+	                else
+	                    timeExtent.setBaseTime(DateTimeFormat.parseIso(val));
+	                
+	                // read lag time
+	                val = dom.getElementValue(extentElt, "lagTime");
+	                if (val != null)
+	                    timeExtent.setLagTimeDelta(Double.parseDouble(val));
+	                
+	                // read lead time
+	                val = dom.getElementValue(extentElt, "leadTime");
+	                if (val != null)
+	                    timeExtent.setLeadTimeDelta(Double.parseDouble(val));
+	                
+	                // read step time
+	                val = dom.getElementValue(extentElt, "stepTime");
+	                if (val != null)
+	                    timeExtent.setTimeStep(Double.parseDouble(val));
+	            
+	                // store that in the hashtable
+	                registerObjectID(dom, extentElt, timeExtent);
+	            }
+	        }
+	        catch (ParseException e)
+	        {
+	            throw new IllegalStateException("Invalid time: " + val);
+	        }
+    	}
+        // read autoUpdate element
+        if (dom.existAttribute(timeElt, "@autoUpdate"))
         {
-            Element extentElt = dom.getElement(timeElt, "TimeExtent");
-            String val = null;            
-            
-            try
-            {    
-                if (extentElt != null)
-                {
-                    // try to get the time extent from the list
-                    Object obj = findExistingObject(dom, extentElt);
-                    if (obj != null)
-                    {
-                        provider.setTimeExtent((STTTimeExtent)obj);
-                        return;
-                    }
-                    
-                    // read base time
-                    val = dom.getElementValue(extentElt, "baseTime");
-                    if (val.equalsIgnoreCase("now"))
-                        timeExtent.setBaseAtNow(true);
-                    else
-                        timeExtent.setBaseTime(DateTimeFormat.parseIso(val));
-                    
-                    // read lag time
-                    val = dom.getElementValue(extentElt, "lagTime");
-                    if (val != null)
-                        timeExtent.setLagTimeDelta(Double.parseDouble(val));
-                    
-                    // read lead time
-                    val = dom.getElementValue(extentElt, "leadTime");
-                    if (val != null)
-                        timeExtent.setLeadTimeDelta(Double.parseDouble(val));
-                    
-                    // read step time
-                    val = dom.getElementValue(extentElt, "stepTime");
-                    if (val != null)
-                        timeExtent.setTimeStep(Double.parseDouble(val));
-                
-                    // store that in the hashtable
-                    registerObjectID(dom, extentElt, timeExtent);
-                }
-            }
-            catch (ParseException e)
-            {
-                throw new IllegalStateException("Invalid time: " + val);
-            }
-            
-            // read autoUpdate element
-            if (dom.existAttribute(timeElt, "@autoUpdate"))
-            {
-                String sceneId = dom.getAttributeValue(timeElt, "@autoUpdate").substring(1);
-                WorldScene scene = (WorldScene)objectIds.get(sceneId);
-                TimeExtentUpdater updater = new SceneTimeUpdater(scene);
-                timeExtent.setUpdater(updater);
-                updater.setEnabled(true);
-            }
+        	String updateVal = dom.getAttributeValue(timeElt, "@autoUpdate");
+        	if(updateVal.startsWith("#")){
+	            String sceneId = updateVal.substring(1);
+	            WorldScene scene = (WorldScene)objectIds.get(sceneId);
+	            TimeExtentUpdater updater = new SceneTimeUpdater(scene);
+	            timeExtent.setUpdater(updater);
+	            updater.setEnabled(true);
+        	} else if("realtime".equalsIgnoreCase(updateVal)){
+        		System.err.println("Realtime MODE");
+        	} else 
+         		throw new IllegalStateException("Invalid value for time attribtue 'autoUpdate': " + updateVal);
         }
+        
+        return timeExtent;
     }
 }
