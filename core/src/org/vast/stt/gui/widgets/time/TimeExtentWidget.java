9a5cc6e674bdb46aa361789706a67598563a5b4e
arobin <arobin@813d399c-aa55-0410-8b7d-c12975737b24>
2007-10-22 20:41:32 +0000

    Updated Copyright Header to MPL v1.1

--- core/src/org/vast/stt/gui/widgets/time/TimeExtentWidget.java
+++ core/src/org/vast/stt/gui/widgets/time/TimeExtentWidget.java
@@ -1,3 +1,28 @@
+/***************************** BEGIN LICENSE BLOCK ***************************
+
+ The contents of this file are subject to the Mozilla Public License Version
+ 1.1 (the "License"); you may not use this file except in compliance with
+ the License. You may obtain a copy of the License at
+ http://www.mozilla.org/MPL/MPL-1.1.html
+ 
+ Software distributed under the License is distributed on an "AS IS" basis,
+ WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ for the specific language governing rights and limitations under the License.
+ 
+ The Original Code is the "Space Time Toolkit".
+ 
+ The Initial Developer of the Original Code is the VAST team at the
+ University of Alabama in Huntsville (UAH). <http://vast.uah.edu>
+ Portions created by the Initial Developer are Copyright (C) 2007
+ the Initial Developer. All Rights Reserved.
+ 
+ Please Contact Mike Botts <mike.botts@uah.edu> for more information.
+ 
+ Contributor(s): 
+    Alexandre Robin <robin@nsstc.uah.edu>    Tony Cook <tcook@nsstc.uah.edu>
+ 
+******************************* END LICENSE BLOCK ***************************/
+
 package org.vast.stt.gui.widgets.time;
 
 import org.eclipse.swt.SWT;
@@ -30,7 +55,7 @@ import org.eclipse.ui.PlatformUI;
  *  next time...  AWT, Swing, SWT- freakin layout managers never change...  TC
  * </p>
  *
- * <p>Copyright (c) 2005</p>
+ * <p>Copyright (c) 2007</p>
  * @author Tony Cook
  * @date Jul 7, 2007
  * @version 1.0
