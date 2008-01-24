620ad160ee43116d23fc06b9549740f9ff52d35f
arobin <arobin@813d399c-aa55-0410-8b7d-c12975737b24>
2008-01-24 18:14:45 +0000

    Added complete support for texture coordinates

--- trunk/core/src/org/vast/stt/style/TexCoordXMapper.java
+++ trunk/core/src/org/vast/stt/style/TexCoordXMapper.java
@@ -25,25 +25,47 @@
 
 package org.vast.stt.style;
 
+import org.vast.cdm.common.DataBlock;
+import org.vast.ows.sld.MappingFunction;
+
 
 /**
  * <p><b>Title:</b><br/>
- * Grid Point Graphic
+ * Texture X Coordinate Mapper
  * </p>
  *
  * <p><b>Description:</b><br/>
- * TODO GridPointGraphic Class Description
+ * This transfers a value from the DataNode to the TX coordinate value.
+ * It will use a mapping function if provided.
  * </p>
  *
  * <p>Copyright (c) 2007</p>
  * @author Alexandre Robin
- * @date Nov 15, 2005
+ * @date Jan 24, 2008
  * @version 1.0
  */
-public class GridPointGraphic extends PrimitiveGraphic
+public class TexCoordXMapper extends PropertyMapper
 {
-    public double tx;
-    public double ty;
-    public double tz;
-    public boolean normalizedTexCoords;
+    GridPointGraphic graphic;
+    
+    
+    public TexCoordXMapper(GridPointGraphic graphic, MappingFunction mappingFunction)
+    {
+        this.graphic = graphic;
+        this.mappingFunction = mappingFunction;
+        if (mappingFunction != null)
+            this.useMappingFunction = true;
+    }    
+
+
+    public void mapData(DataBlock data)
+    {
+        if (useMappingFunction)
+        {
+            double val = data.getDoubleValue();
+            graphic.tx = mappingFunction.compute(val);
+        }
+        else
+            graphic.tx = data.getDoubleValue();     
+    }    
 }
