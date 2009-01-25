a5d7c3681b6c1bc12a7e15a1ff4a16ad85efa8c7
alex.robin@sensiasoftware.com <alex.robin@sensiasoftware.com@813d399c-aa55-0410-8b7d-c12975737b24>
2009-01-25 18:08:30 +0000

    Removed reference to deprecated IPlatformRunnable

--- core/src/org/vast/stt/apps/Application.java
+++ core/src/org/vast/stt/apps/Application.java
@@ -25,7 +25,8 @@
 
 package org.vast.stt.apps;
 
-import org.eclipse.core.runtime.IPlatformRunnable;
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.PlatformUI;
 
@@ -33,29 +34,33 @@ import org.eclipse.ui.PlatformUI;
 /**
  * This class controls all aspects of the application's execution
  */
-public class Application implements IPlatformRunnable
+public class Application implements IApplication
 {
 
-	/* (non-Javadoc)
-	 * @see org.eclipse.core.runtime.IPlatformRunnable#run(java.lang.Object)
-	 */
-	public Object run(Object args) throws Exception
+	public Object start(IApplicationContext appCtx) throws Exception
 	{
 		Display display = PlatformUI.createDisplay();
+		
 		try
 		{
 			int returnCode = PlatformUI.createAndRunWorkbench(display, new ApplicationWorkbenchAdvisor());
 			
 			if (returnCode == PlatformUI.RETURN_RESTART)
 			{
-				return IPlatformRunnable.EXIT_RESTART;
+				return IApplication.EXIT_RESTART;
 			}        
 	        
-			return IPlatformRunnable.EXIT_OK;
+			return IApplication.EXIT_OK;
 		}
 		finally
 		{
 			display.dispose();
 		}
+	}
+
+	
+	public void stop()
+	{
+		PlatformUI.getWorkbench().close();
 	}
 }
