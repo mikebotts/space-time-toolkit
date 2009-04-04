c8752500c77cc3c4fc04b28232a7d1f973c9359e
tcook@nsstc.uah.edu <tcook@nsstc.uah.edu@813d399c-aa55-0410-8b7d-c12975737b24>
2009-04-04 00:19:25 +0000

    Added ProgresMonitorDialog to projectLoading.  It allows user to cancel, and seems to work okay, but I need a couple of other folks to test it!  Also removed "OpenTestProject" option from menu.

--- core/src/org/vast/stt/commands/OpenProject.java
+++ core/src/org/vast/stt/commands/OpenProject.java
@@ -25,23 +25,125 @@
 
 package org.vast.stt.commands;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.WorkbenchException;
+import org.vast.stt.gui.dialogs.DataProviderJob;
+import org.vast.stt.gui.views.ScenePageInput;
 import org.vast.stt.project.Project;
 import org.vast.stt.project.ProjectReader;
+import org.vast.stt.project.STTDisplay;
+import org.vast.stt.project.scene.Scene;
+import org.vast.stt.project.tree.DataItem;
+import org.vast.stt.project.tree.DataItemIterator;
+import org.vast.stt.project.world.WorldScene;
+import org.vast.stt.provider.DataProvider;
 
 
 public class OpenProject implements Command
 {
 	private String url = null;
     private Project project = null;
+    private boolean isUpdating = false;
+    private boolean sceneLoading = false;
     
-	
+    private class LoadProjectRunnable implements  IRunnableWithProgress
+	{
+		ProjectReader reader;
+
+		public LoadProjectRunnable(ProjectReader pr)
+		{
+			reader = pr;
+		}
+		
+		public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+			String msg = "Attempting to open project: " + url + "...";
+
+			try 
+			{
+				monitor.beginTask(msg, IProgressMonitor.UNKNOWN);
+				monitor.subTask("ReadingProject...");
+				reader.readProject(url);
+				while(isUpdating){
+					try {
+						Thread.sleep(40);
+						if (monitor.isCanceled()) {
+							isUpdating =  false;
+						}
+						if(reader.getProject() != null)
+							isUpdating = false;
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+//						System.err.println("LPR.run.IntrptdEx");
+						e.printStackTrace();
+					} finally {
+						isUpdating = false;
+					}
+				}
+				if(monitor.isCanceled()) {
+					reader.cancelRead();
+					throw new InterruptedException("Project Loading Cancelled");
+				}
+				monitor.subTask("Loading SceneTree...");
+				Runnable openPage = new OpenPageRunnable(reader.getProject());
+				PlatformUI.getWorkbench().getDisplay().asyncExec(openPage);
+				sceneLoading = true;
+				while(sceneLoading){
+					try {
+						Thread.sleep(40);
+						//  Allowing OpenPageRunnable to cancel currently will cause some problems
+						//  Needs work, just diable cancel for now.
+						if (monitor.isCanceled()) {
+							sceneLoading = false;
+						}
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			} finally {
+				if(monitor.isCanceled()) {
+					Project proj = reader.getProject();
+					proj = null;
+					throw new InterruptedException("Project Loading Cancelled");
+				}				monitor.done();
+				sceneLoading = false;
+			}		  
+		}
+					
+	}
+		
 	public void execute()
 	{
+		Display display = PlatformUI.getWorkbench().getDisplay();
+		Shell shell = Display.getDefault().getActiveShell();
+		ProgressMonitorDialog pmd = new ProgressMonitorDialog(shell);
+		pmd.setCancelable(true);
 		ProjectReader reader = new ProjectReader();
-		project = reader.readProject(url);
-        System.gc();
+		LoadProjectRunnable lpr = new LoadProjectRunnable(reader);
+		try {
+			pmd.run(true,true, lpr);
+		} catch (InvocationTargetException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+	        MessageDialog.openInformation(shell, "Information", "User cancelled Project Loading");
+	        e1.printStackTrace();
+		}
+		
+        System.gc();  //  Check me...
 	}
-    
 
 	public void unexecute()
 	{
@@ -70,4 +172,74 @@ public class OpenProject implements Command
     {
         return project;
     }
+    
+    public boolean isUpdating()
+	{
+    	return isUpdating;
+	}
+    
+    public class OpenPageRunnable implements Runnable
+    {
+        private Project project;
+        private boolean cancelled = false;
+        
+        public OpenPageRunnable(Project project)
+        {
+            this.project = project;
+        }
+        
+        public void run()
+        {
+            try
+            {
+                ArrayList<STTDisplay> displayList = project.getDisplayList();
+                for (int i=0; i<displayList.size(); i++)
+                {
+                    STTDisplay nextDisplay = displayList.get(i);
+                    
+                    // open a new page  a WorldScene
+                    if (nextDisplay instanceof WorldScene)
+                    {
+                        WorldScene scene = (WorldScene)nextDisplay;
+                        
+                        // keep list of providers we did so we don't do them twice
+                        List<DataProvider> processedProviders = new ArrayList<DataProvider>(20);
+                        
+                        // add job progress listener to all providers
+                        DataItemIterator it = ((Scene)nextDisplay).getDataTree().getItemIterator();
+                        while (it.hasNext())
+                        {
+                        	DataItem item = it.next();
+                            DataProvider provider = item.getDataProvider();
+                            if (!processedProviders.contains(provider))
+                            {
+                                if (provider.getSpatialExtent().getUpdater() == null)
+                                    new DataProviderJob(provider);
+                                processedProviders.add(provider);
+                            }
+                        }
+//                        
+                        // create scene page input and open new page
+                        ScenePageInput pageInput = new ScenePageInput(scene);
+                        IWorkbenchWindow oldWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+                        oldWindow.openPage("STT.Perspective", pageInput);
+                        
+                        // close empty page
+                        if (oldWindow.getActivePage().getInput() == null)
+                            oldWindow.close();
+                    }
+                }
+            }
+            catch (WorkbenchException e)
+            {
+                e.printStackTrace();
+            }
+            finally {
+            	sceneLoading = false;
+            }
+        }
+    };
+    
+   
+    
 }
